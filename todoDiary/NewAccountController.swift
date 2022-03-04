4fe31710206b9d7927f3a2cc151aa1852cff175f
jooeun kim <jay311311@gmail.com>
2022-03-05 00:55:05 +0900

    Registration_비밀번호 유효성검사 로직 및 ui 구현

--- todoDiary/NewAccountController.swift
+++ todoDiary/NewAccountController.swift
@@ -16,19 +16,48 @@ class NewAccountController: UIViewController {
     @IBOutlet weak var IdTextField: UITextField!
     @IBOutlet weak var PasswordTextField: UITextField!
     @IBOutlet weak var NameTextField: UITextField!
-    
     @IBOutlet weak var signUpBtn: UIButton!
     
+    @IBOutlet weak var idErrorMessage: UILabel!
+    @IBOutlet weak var passwordErrorMessage: UILabel!
+    @IBOutlet weak var nameErrorMessage: UILabel!
+    
+    var validationPassword :String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         TextFieldBox.layer.cornerRadius=30
-     
+        idErrorMessage.text = ""
+        passwordErrorMessage.text = ""
+        nameErrorMessage.text=""
+        
         signUpBtn.addTarget(self, action: #selector(touchSignup), for: .touchUpInside)
     }
     
+    
+    
    
-
+    @IBAction func checkValidationPassword(_ sender: UITextField) {
+        
+        guard let password = PasswordTextField.text else { return }
+        let rexPassword = NSPredicate(format: "SELF MATCHES %@ ", "^(?=.*[a-z])(?=.*[0-9])(?=.*[$@$#!%*?&])(?=.*[A-Z]).{6,}$")
+        if rexPassword.evaluate(with: password) {
+            PasswordTextField.layer.borderColor = UIColor.green.cgColor
+            PasswordTextField.layer.borderWidth = 0.25
+            PasswordTextField.layer.cornerRadius = 5.0
+            passwordErrorMessage.textColor = UIColor.red
+            passwordErrorMessage.text = ""
+            validationPassword = password
+        }else{
+            PasswordTextField.layer.borderColor = UIColor.red.cgColor
+            PasswordTextField.layer.cornerRadius = 5.0
+            PasswordTextField.layer.borderWidth = 0.25
+            passwordErrorMessage.textColor = UIColor.red
+            passwordErrorMessage.text = "최소 6자 이상 & 영어 대소문자,숫자,특수기호를 포함하여주세요"
+        }
+        
+    }
+    
     
     
     @objc func touchSignup(){
