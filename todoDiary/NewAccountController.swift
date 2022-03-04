b809b213c463a6389b391ea5e76e91ec93e3ae7d
jooeun kim <jay311311@gmail.com>
2022-03-04 17:49:01 +0900

    Registration_유저 계정 생성

--- todoDiary/NewAccountController.swift
+++ todoDiary/NewAccountController.swift
@@ -6,19 +6,50 @@
 //
 
 import UIKit
+import Firebase
+
 
 class NewAccountController: UIViewController {
-    let vc =  UIViewController()
 
     @IBOutlet weak var TextFieldBox: UIView!
+    
+    @IBOutlet weak var IdTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var NameTextField: UITextField!
+    
+    @IBOutlet weak var signUpBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //vc.modalPresentationStyle = .fullScreen
         TextFieldBox.layer.cornerRadius=30
      
-        // Do any additional setup after loading the view.
+        signUpBtn.addTarget(self, action: #selector(touchSignup), for: .touchUpInside)
+    }
+    
+   
+
+    
+    
+    @objc func touchSignup(){
+        guard let email =  IdTextField.text, let password =  PasswordTextField.text  else {
+           return  print("정보를 빠짐없이 기입해 주세요")
+        }
+        
+        Auth.auth().createUser(withEmail:email, password: password) { authResult, error in
+            if authResult != nil {
+                print("signup success")
+            }else{
+                print("signup fail---> \(error?.localizedDescription)")
+
+            }
+        }
     }
     
 
 
+}
+
+struct User {
+    
 }
