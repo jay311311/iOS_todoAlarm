caf16c65e7cb5b253669704a60adaed406809246
jooeun kim <jay311311@gmail.com>
2022-03-05 01:50:55 +0900

    Registraion_ 이메일 형식 유효성 검사 로직 및 ui 구현 + 모든 textField 입력시 계정 생성

--- todoDiary/NewAccountController.swift
+++ todoDiary/NewAccountController.swift
@@ -10,7 +10,7 @@ import Firebase
 
 
 class NewAccountController: UIViewController {
-
+    
     @IBOutlet weak var TextFieldBox: UIView!
     
     @IBOutlet weak var IdTextField: UITextField!
@@ -22,7 +22,9 @@ class NewAccountController: UIViewController {
     @IBOutlet weak var passwordErrorMessage: UILabel!
     @IBOutlet weak var nameErrorMessage: UILabel!
     
+    var validationId : String = ""
     var validationPassword :String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,50 +37,76 @@ class NewAccountController: UIViewController {
     }
     
     
+    @IBAction func checkValidationId(_ sender: Any) {
+        IdTextField.layer.cornerRadius = 5.0
+        
+        guard let id = IdTextField.text else { return }
+        let rexId = NSPredicate(format: "SELF MATCHES %@ ", "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,8}$")
+        if rexId.evaluate(with: id){
+            IdTextField.layer.borderColor = UIColor.green.cgColor
+            IdTextField.layer.borderWidth = 0.25
+            idErrorMessage.text = ""
+            validationId = id
+        }else{
+            IdTextField.layer.borderColor = UIColor.red.cgColor
+            IdTextField.layer.borderWidth = 0.25
+            idErrorMessage.textColor = UIColor.red
+            idErrorMessage.text = "* 이메일형식을 확인해주세요"
+        }
+        
+    }
     
-   
+    
     @IBAction func checkValidationPassword(_ sender: UITextField) {
+        PasswordTextField.layer.cornerRadius = 5.0
         
         guard let password = PasswordTextField.text else { return }
         let rexPassword = NSPredicate(format: "SELF MATCHES %@ ", "^(?=.*[a-z])(?=.*[0-9])(?=.*[$@$#!%*?&])(?=.*[A-Z]).{6,}$")
         if rexPassword.evaluate(with: password) {
             PasswordTextField.layer.borderColor = UIColor.green.cgColor
             PasswordTextField.layer.borderWidth = 0.25
-            PasswordTextField.layer.cornerRadius = 5.0
-            passwordErrorMessage.textColor = UIColor.red
             passwordErrorMessage.text = ""
             validationPassword = password
         }else{
             PasswordTextField.layer.borderColor = UIColor.red.cgColor
-            PasswordTextField.layer.cornerRadius = 5.0
             PasswordTextField.layer.borderWidth = 0.25
             passwordErrorMessage.textColor = UIColor.red
-            passwordErrorMessage.text = "최소 6자 이상 & 영어 대소문자,숫자,특수기호를 포함하여주세요"
+            passwordErrorMessage.text = "* 최소 6자 이상 & 영어 대소문자,숫자,특수기호를 포함하여주세요"
         }
         
     }
     
+    @IBAction func checkName(_ sender: Any) {
+        
+        NameTextField.layer.borderColor = UIColor.green.cgColor
+        NameTextField.layer.borderWidth = 0.25
+        NameTextField.layer.cornerRadius = 5.0
+        nameErrorMessage.textColor = UIColor.red
+        nameErrorMessage.text = ""
+    }
     
     
     @objc func touchSignup(){
-        guard let email =  IdTextField.text, let password =  PasswordTextField.text  else {
-           return  print("정보를 빠짐없이 기입해 주세요")
-        }
-        
-        Auth.auth().createUser(withEmail:email, password: password) { authResult, error in
-            if authResult != nil {
-                print("signup success")
-            }else{
-                print("signup fail---> \(error?.localizedDescription)")
-
+        if validationId.count <= 0 || validationPassword.count <= 0 ||
+            NameTextField.text?.isEmpty != nil {
+            
+            let alert =   UIAlertController(title: "알림", message: "정보를 기입해 주세요", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style:.default , handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            NameTextField.layer.borderColor = UIColor.red.cgColor
+            NameTextField.layer.borderWidth = 0.25
+            nameErrorMessage.textColor = UIColor.red
+            nameErrorMessage.text = "* 이름을 입력하여 주세요"
+        }else{
+            Auth.auth().createUser(withEmail:validationId, password: validationPassword) { authResult, error in
+                if authResult != nil {
+                    print("signup success")
+                }else{
+                    print("signup fail---> \(error?.localizedDescription)")
+                }
             }
         }
     }
-    
-
-
 }
 
-struct User {
-    
-}
+
