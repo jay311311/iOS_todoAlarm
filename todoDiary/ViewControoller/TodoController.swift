56cc1ecc0d97f7d4ffbd2b00b287a91a17c4fbc6
jooeun kim <jay311311@gmail.com>
2022-04-11 22:53:37 +0900

    Todo_ cell에 투두 데이트 추가 + todo 추가하는 페이지 삭제 및 투두 삽입 부분 변경

--- todoDiary/ViewControoller/TodoController.swift
+++ todoDiary/ViewControoller/TodoController.swift
@@ -11,10 +11,7 @@ import UIKit
 
 class TodoController: UIViewController {
     var i  = 0
-//    let db =  Database.database().reference().child("user")
-//    let userUid =  Auth.auth().currentUser?.uid
-  //  var todos = [Todo]()
-  //  var textTitle = ""
+
     
     @IBOutlet weak var todoCollectionVIew: UICollectionView!
     let todoListViewModel = TodoViewModel()
@@ -24,69 +21,15 @@ class TodoController: UIViewController {
         todoListViewModel.loadTask()
        
 
-//        let todo = TodoManager.shared.createTodo(title: "first", date: "2022-10-20")
-//        Storage.store(todo, to: .documents, as: "todo.json")
-        
-           //   Storage.store(<#T##obj: Encodable##Encodable#>, to: <#T##Storage.Directory#>, as: <#T##String#>)(todo, fileName: "todo.json")
-     //   fetchData()
+
     }
     
-//    func fetchData(){
-//        todos.removeAll()
-//       db.child("\(userUid!)").child("todos").observe(.childAdded) { (snapshot) in
-//            do{
-//                guard let myTodos =  snapshot.value as? [String : Any] else {return}
-//                let decoder =  JSONDecoder()
-//                let data =  try JSONSerialization.data(withJSONObject: myTodos, options: [])
-//                let todo = try decoder.decode(Todo.self, from: data)
-//                self.todos.append(todo)
-//                print("시작 \(self.todos)")
-//                DispatchQueue.main.async {
-//                    self.todoCollectionVIew.reloadData()
-//                }
-//            }catch let error{
-//                print(String(describing: error))
-//            }
-//        }
-//    }
-//
+
     @IBAction func openModal(_ sender: UIButton) {
         performSegue(withIdentifier: "addTodo", sender: nil)
     }
     
-//    func dataUpdate(id: String?, element:Int?, category:String? ) {
-//        guard let id = id, let element = element, let category = category else { return }
-//       // db.child("\(userUid!)").child("todos").child(id).child(category).setValue(element)
-//    }
-//
-//    func dataDelete(id: String?, tag: Int?) {
-//        guard let id = id, let tag = tag else { return }
-//        print("파이어 베이스 삭제 로드 / id : \(id), tag :\(tag)")
-//        print(self.todos)
-//       // db.child("\(userUid!)").child("todos").child(id).removeValue()
-//       self.todos.remove(at: tag)
-//
-//        DispatchQueue.main.async {
-//        self.todoCollectionVIew.reloadData()
-//
-//            for views in  self.todoCollectionVIew.subviews {
-//
-//
-//                let viewWidth =  views.bounds.size.width
-//                let viewHeight = views.bounds.size.height
-//                for view in views.subviews{
-//                    print("todoview \(view)")
-//                    print("\(viewWidth)& \(viewHeight)")
-//                    view.frame =  CGRect(x: 0.0, y: 0.0, width: viewWidth, height: viewHeight)
-//                    print("================\(view)================")
-//                }
-//            }
-//            self.todoCollectionVIew.reloadData()
-////            print("완료")
-//        }
-//
-//    }
-//
+
 }
 
 extension TodoController : UICollectionViewDataSource,  UICollectionViewDelegate{
@@ -94,23 +37,11 @@ extension TodoController : UICollectionViewDataSource,  UICollectionViewDelegate
         return todoListViewModel.numOfSection
     }
     
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        //todo : 섹션 아이템 몇개
-//        if section == 0 {
-//           return todoListViewModel.todayTodos.count
-//        }else {
-//            return todoListViewModel.laterTodos.count
-//        }
-//
-//
-//    }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? TodoCellController else {
             return UICollectionViewCell()
         }
-//let todoData =  todos[indexPath.row]
-        // - 데이터 fetch
        // cell.cellTitle.text = todoData.todo_title
         var todo : Todo
         todo = todoListViewModel.todos[indexPath.item]
@@ -215,9 +146,10 @@ extension TodoController : UICollectionViewDelegateFlowLayout{
 
 class TodoCellController: UICollectionViewCell  {
     
+    @IBOutlet weak var cellDate: UILabel!
     @IBOutlet weak var swipeBox: UIStackView!
     @IBOutlet weak var cellBox : UIView!
-    @IBOutlet weak var hashLabelBox: UIStackView!
+//    @IBOutlet weak var hashLabelBox: UIStackView!
     @IBOutlet weak var cellTitle: UILabel!
     @IBOutlet weak var star: UIButton!
     @IBOutlet weak var bell: UIButton!
@@ -241,6 +173,7 @@ class TodoCellController: UICollectionViewCell  {
     func updateUI(todo : Todo){
         // 셀 업데이트 하기
         checkBox.isSelected = todo.isDone
+        cellDate.text = todo.date
         cellTitle.text = todo.title
         cellTitle.alpha = todo.isDone ? 0.2 : 1
         deleteBtn.isHidden =  todo.isDone == false
---
+++ todoDiary/ViewControoller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  todoDiary
+//
+//  Created by Jooeun Kim on 2022/03/03.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+    }
+
+ 
+    
+    
+}
+
